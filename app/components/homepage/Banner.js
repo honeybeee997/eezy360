c1ae925e0f4dba0b279a239c4b9b65ec9004df1f
Muneeb <ahmedmuneeb997@gmail.com>
2024-09-15 13:55:47 +0500

    FEAT: Homepage Banner V1

--- app/components/homepage/Banner.js
+++ app/components/homepage/Banner.js
@@ -5,13 +5,15 @@ import Image from "next/image";
 import Text from "@/typography/Text";
 import Button from "@/components/ui/Button";
 
+import classes from "./Banner.module.css";
+
 const HomepageBanner = () => {
   return (
     <section className="h-screen flex items-end home-banner overflow-hidden relative">
       <Image
         src="/assets/home-banner-bg.webp"
         alt="eezy360 banner-bg"
-        className="opacity-15 absolute z-[-1] top-[-10%] right-[-33%]"
+        className={`opacity-15 absolute z-[-1] top-[-10%] right-[-33%] ${classes.img}`}
         width={1920}
         height={1080}
       />
@@ -22,6 +24,10 @@ const HomepageBanner = () => {
             creative
           </Text>{" "}
           spark behind your ads.
+        </Text>
+        <Text el="p" className="max-w-5xl text-2xl">
+          Get more more customers using content creation and social media
+          marketing
         </Text>
         <Button>Book a Free Demo</Button>
       </div>
