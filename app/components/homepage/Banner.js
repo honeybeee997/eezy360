82e7c8124bcd555c232a7a4e89a15b5e33ab67ad
Muneeb <ahmedmuneeb997@gmail.com>
2024-09-15 14:07:13 +0500

    FEAT: Homepage Banner V3

--- app/components/homepage/Banner.js
+++ app/components/homepage/Banner.js
@@ -9,13 +9,14 @@ import classes from "./Banner.module.css";
 const HomepageBanner = () => {
   return (
     <section
-      className={`h-screen flex items-end  overflow-hidden relative ${classes.home_banner}`}
+      className={`h-screen flex items-center justify-center  overflow-hidden relative ${classes.home_banner}`}
     >
       <Video
         src="/assets/file.mp4"
-        className="absolute top-0 left-0 w-full h-full object-cover z-[-1] opacity-70 scale-[1.2]"
+        className="absolute top-0 left-0 w-full h-full object-cover z-[-1] opacity-50 scale-[1.2]"
       />
-      <div className="p-10 max-w-5xl bg-black backdrop-blur-sm bg-opacity-10 rounded-tr-xl [text-shadow:_0_2px_0_rgb(0_0_0/_0.5)]">
+      {/* Height of the header(84px) + top offset(20px) = 104px margin-top  mt-[104px] */}
+      <div className="p-10 max-w-5xl rounded-xl text-center [text-shadow:_0_1px_0_rgb(0_0_0/_0.5)]">
         <Text el="h1" className="text-8xl [text-shadow:none]">
           The{" "}
           <Text el="span" className="text-8xl outlined-text">
